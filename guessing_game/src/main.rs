3bd5f2951ca2bb86b9c16d5b0cb78ff3330dd386
ksks2012 <st890751999@gmail.com>
2022-10-16 11:58:23 +0800

    feat(guessing_game): add rand to gen secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,7 @@
 // import standard library
 use std::io;
+// import rand, set dependencies in Cargo.toml
+use rand::Rng;
 
 // entry point
 fn main() {
@@ -7,6 +9,11 @@ fn main() {
 
     println!("Please input your guess.");
 
+    // rand number between (1, 100)
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {secret_number}");
+    
     // create a variable with mut (mutable)
     // '::' means associated function new() is in String
     let mut guess = String::new();
