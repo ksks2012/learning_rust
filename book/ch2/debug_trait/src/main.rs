1ebd81c9d10c4c53358d97b595764247ddb402c0
ksks2012 <st890751999@gmail.com>
2023-07-16 01:48:03 +0800

    feat(ch2): Add debug_trait
    - Debug for Point structure

---
+++ book/ch2/debug_trait/src/main.rs
@@ -0,0 +1,16 @@
+use std::fmt::*;
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Debug for Point {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    let origin = Point { x: 0, y: 0 };
+    println!("{:?}", origin);
+}
